55508bb439d2d43bf3c373a5aae7cbc5c01886cc
ofentsejohndifeto <ofentsejohnd22@gmail.com>
2025-09-03 07:00:10 +0200

    before App crunning dapp

--- truffle.js
+++ truffle.js
@@ -1,19 +1,18 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
+      host: "127.0.0.1",     // Localhost (Ganache)
+      port: 8545,            // Default Ganache port
+      network_id: "*",       // Match any network id
+                             // Optional: increase gas limit
     }
   },
+
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.4.24"     // Match your Solidity version
     }
   }
 };
